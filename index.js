65b24fe300d3d6cef25bba6e7a4b813e5c16ed2e
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2025-05-03 23:00:10 -0400

    Desafio piedra papel o tijeras

--- index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const month = new Date().toJSON().slice(0, 7);
-console.log(month);
---
+++ index.ts
@@ -0,0 +1,31 @@
+//Crear piedra papel o tijera
+/* 
+La terminal muestra al usuario un mensaje de bienvenida y le pregunta si quiere jugar
+o conocer las reglas del juego 
+
+Si el usuario elige jugar, se le pide que ingrese la cantidad de partidas a jugar y luego
+se le solicita que elija entre piedra, papel o tijera.
+El usuario elige una opción y la computadora elige una opción aleatoria.
+El usuario gana si:
+- Elige piedra y la computadora elige tijera
+- Elige papel y la computadora elige piedra
+- Elige tijera y la computadora elige papel
+El usuario pierde si:
+- Elige piedra y la computadora elige papel
+- Elige papel y la computadora elige tijera
+- Elige tijera y la computadora elige piedra
+El usuario empata si:
+- Elige piedra y la computadora elige piedra
+- Elige papel y la computadora elige papel
+- Elige tijera y la computadora elige tijera
+
+Al final de las partidas se muestra el resultado de cada partida y el resultado final
+El resultado de cada partida es: 
+- Ganaste
+- Perdiste
+- Empate
+El resultado final es:
+- Ganaste X partidas
+- Perdiste X partidas
+- Empataste X partidas
+*/
